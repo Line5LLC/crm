d7c3f4e0cbd3622ca2c90a8ec0dfec38f8195679
Luan Junior <luans@line5.com>
2024-04-08 19:25:09 -0300

    Removing logger test

--- app/controllers/single_sign_ons_controller.rb
+++ app/controllers/single_sign_ons_controller.rb
@@ -5,21 +5,13 @@ class SingleSignOnsController < ApplicationController
   layout false
 
   def create
-    Rails.logger.error(".....TESTE.....")
-
     token = cookies[:crm_sso_token]
-
-    Rails.logger.error(token)
 
     user = User.find_by(sso_token: token) if token
 
-    Rails.logger.error(user.sso_token)
-    Rails.logger.error(user.id)
     if user
       sign_in(user)
       after_sign_in_path_for(user)
-    else
-      Rails.logger.error("Token inválido ou usuário não encontrado.")
     end
     redirect_to root_path
   end
