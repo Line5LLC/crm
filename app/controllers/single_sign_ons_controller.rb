c61a8425a5e36a8cda1104be22e999e655147ba1
Luan Junior <luans@line5.com>
2024-04-08 15:16:55 -0300

    Test

--- app/controllers/single_sign_ons_controller.rb
+++ app/controllers/single_sign_ons_controller.rb
@@ -5,19 +5,21 @@ class SingleSignOnsController < ApplicationController
   layout false
 
   def create
+    Rails.logger.error(".....TESTE.....")
+
     token = cookies[:crm_sso_token]
 
-    flash[:alert] = token
+    Rails.logger.error(token)
 
     user = User.find_by(sso_token: token) if token
 
-    flash[:alert] = user.sso_token
-    flash[:alert] = user.id
+    Rails.logger.error(user.sso_token)
+    Rails.logger.error(user.id)
     if user
       sign_in(user)
       after_sign_in_path_for(user)
     else
-      flash[:alert] = "Token inválido ou usuário não encontrado."
+      Rails.logger.error("Token inválido ou usuário não encontrado.")
     end
     redirect_to root_path
   end
